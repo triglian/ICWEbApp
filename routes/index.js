1e4a82c5835690cc8370dcf717fa0f1943966725
Luigi Frunzio <Giggiux@Luigis-MacBook-Pro.local>
2015-11-19 22:46:39 +0100

    New Menu News section Responsive Menu New Dropdown element

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { message: 'Express' });
+  res.render('index', { message: 'ICWE2016' });
 });
 
 module.exports = router;
