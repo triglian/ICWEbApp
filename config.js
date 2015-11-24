6043ea52607e0b7cb8c20d0feb623a8028dcf50d
Luigi Frunzio <giggiux@gmail.com>
2015-11-24 14:29:08 +0100

    Added Mongoose

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Giggiux on 24/11/15.
+ */
+module.exports = {
+    // MongoDB
+    // Url of the Mongodb server
+    mongoUrl: "mongodb://localhost/",
+    // Database name
+    mongoDbName: "ICWEbApp",
+    //Server URL
+    url: "http://localhost:3000",
+    //TITLE
+    title: "ICWE2016"
+}
