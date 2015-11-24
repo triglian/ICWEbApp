ab27625693797044764ef0a44136e4eb586555e0
Luigi Frunzio <giggiux@gmail.com>
2015-11-24 21:28:43 +0100

    Removed Dust.js, Now we only use Polymer

--- routes/root/router.js
+++ routes/root/router.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var config = require('../../config');
 var express = require('express');
 var router = express.Router();
 var middleware =  require('../middleware');
@@ -9,7 +9,15 @@ router.all('/', middleware.supportedMethods('GET, OPTIONS'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+    res.render('index')
 });
 
+/*GET for basic info about the conference */
+router.get('/info/', function(req,res,next){
+   var info = {
+       'url': config.url,
+       'title': config.title
+   };
+    res.json(info)
+});
 module.exports = router;
