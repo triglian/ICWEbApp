aef9bf20b93588f59fdc793a160011be0f1f82bd
Luigi Frunzio <giggiux@gmail.com>
2015-11-30 12:56:08 +0100

    finished merging

--- routes/Events/router.js
+++ routes/Events/router.js
@@ -7,6 +7,8 @@ var middleware =  require('../middleware');
 var mongoose = require('mongoose');
 //var ObjectId = mongoose.Types.ObjectId;
 var Event = mongoose.model('Event');
+var Comment = mongoose.model('Comment');
+
 
 var config = require("../../config");
 
@@ -40,21 +42,71 @@ router.get('/:eventid', function(req, res, next) {
     });
 });
 
-router.post('/:eventid/feedback', function(req, res, next){
-    console.log(req.body);
-    res.status(200);
-    res.json({
-        statusCode: 200,
-        message: "OK"
-    });
-});
-
 router.get('/:eventid/feedback', function(req, res, next) {
-    res.status(200);
-    res.json({
-        statusCode: 200,
-        message: "OK"
+    Event.findById(req.params.eventid, fieldsFilter , function(err, event) {
+        if (err) return next(err);
+        if (!event) {
+            res.status(404);
+            res.json({
+                statusCode: 404,
+                message: "Not Found"
+            });
+        }
+        else {
+            res.json(event.feedback);
+        }
+    })
+
+});
+
+router.post('/:eventid/feedback', function(req, res, next){
+    var data = req.body;
+    Event.findById(req.params.eventid, fieldsFilter , function(err, event){
+        if (err) return next (err);
+        if (!event) {
+            res.status(404);
+            res.json({
+                statusCode: 404,
+                message: "Not Found"
+            });
+            return;
+        }
+        var feed = new Comment();
+        feed.email = data.email;
+        feed.stars = data.stars;
+        feed.comment = data.comment;
+
+        event.feedback.push(feed);
+        event.save(onModelSave(res));
     });
 });
 
+function onModelSave(res, status, sendItAsResponse){
+    var statusCode = status || 204;
+    var sendItAsResponse = sendItAsResponse || false;
+    return function(err, saved){
+        if (err) {
+            if (err.name === 'ValidationError'
+                || err.name === 'TypeError' ) {
+                res.status(400)
+                return res.json({
+                    statusCode: 400,
+                    message: "Bad Request"
+                });
+            }else{
+                return next (err);
+            }
+        }
+        if( sendItAsResponse){
+            var obj = saved.toObject();
+            delete obj.password;
+            delete obj.__v;
+            addLinks(obj);
+            res.status(statusCode)
+            return res.json(obj);
+        }else{
+            return res.status(statusCode).end();
+        }
+    }
+}
 module.exports = router;
