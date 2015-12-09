3e323c77d8f79c7236ef326b560e237da2e54cfd
Samuele Decarli <samuele.decarli@usi.ch>
2015-12-09 14:26:13 +0100

    Factored out date sorting function

--- public/js/utils.js
+++ public/js/utils.js
@@ -2,3 +2,9 @@ function formatDateSingle(date) {
     var d = new Date(date);
     return d.toDateString().slice(3, 10) + " at " + d.toTimeString().slice(0, 5);
 }
+
+function sortDatesDescending(a, b) {
+    var dateA = new Date(a.date);
+    var dateB = new Date(b.date);
+    return dateB.getTime() - dateA.getTime()
+}
