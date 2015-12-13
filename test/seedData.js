3d2801494312169d4be64c4b118ea3c840d80732
Federico Pfahler <pfahler.federico@gmail.com>
2015-12-13 13:00:18 +0100

    Added return carriage for abstract and bio

--- test/seedData.js
+++ test/seedData.js
@@ -15,13 +15,13 @@ var program = {
       speakers  : [{
                 name          : "Marco Calzana",
                 organisation  : "USI",
-                bio           : "Curabitur eu odio nec massa elementum ultrices egestas a sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec et ipsum ut nisl efficitur congue. Praesent elementum, ligula quis euismod maximus, tellus ipsum dignissim lacus, at tincidunt erat diam quis tortor. Suspendisse non neque interdum, tincidunt dolor vel, pharetra libero. Phasellus accumsan mattis viverra. Mauris velit lorem, tempor nec ipsum at, condimentum finibus ligula. Fusce sollicitudin, eros eget porta consequat, enim ipsum pretium ligula, non iaculis ipsum massa in velit. Proin nulla enim, euismod elementum sem id, congue sagittis purus. Praesent sem augue, fringilla eu sodales quis, dapibus vitae nisl.In lacus est, lobortis nec suscipit a, imperdiet nec libero. Nulla a nibh tincidunt, placerat justo nec, maximus ante. Cras eget mi augue. Vestibulum blandit interdum velit, vitae egestas nisl euismod quis. In vitae velit sit amet odio suscipit finibus sit amet quis urna. Aenean feugiat erat quis varius cursus. Sed convallis sem eget nisl malesuada imperdiet. Cras vitae mauris enim. Curabitur tristique efficitur orci, vitae rutrum diam finibus vel.",
+                bio           : "Curabitur eu odio nec massa elementum ultrices egestas a sapien.\n Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec et ipsum ut nisl efficitur congue.\n Praesent elementum, ligula quis euismod maximus, tellus ipsum dignissim lacus, at tincidunt erat diam quis tortor. Suspendisse non neque interdum, tincidunt dolor vel, pharetra libero. Phasellus accumsan mattis viverra. Mauris velit lorem, tempor nec ipsum at, condimentum finibus ligula. Fusce sollicitudin, eros eget porta consequat, enim ipsum pretium ligula, non iaculis ipsum massa in velit. Proin nulla enim, euismod elementum sem id, congue sagittis purus. Praesent sem augue, fringilla eu sodales quis, dapibus vitae nisl.In lacus est, lobortis nec suscipit a, imperdiet nec libero. Nulla a nibh tincidunt, placerat justo nec, maximus ante. Cras eget mi augue. Vestibulum blandit interdum velit, vitae egestas nisl euismod quis. In vitae velit sit amet odio suscipit finibus sit amet quis urna. Aenean feugiat erat quis varius cursus. Sed convallis sem eget nisl malesuada imperdiet. Cras vitae mauris enim. Curabitur tristique efficitur orci, vitae rutrum diam finibus vel.",
                 website       : "www.marcocalzana.ch",
                 twitter       : "@marcoRocket",
                 picture       : "",
                 email         : "marco@marco.ch"
       },{ref:"Luigi Frunzio"}],
-      abstract : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec neque tempor velit sollicitudin dignissim. Morbi sem velit, finibus et nulla id, tempor hendrerit enim. Maecenas sit amet dolor tincidunt, aliquet orci et, bibendum mi. In placerat suscipit risus a dictum. Cras ut tortor mauris. Proin arcu odio, ornare sit amet mattis faucibus, laoreet sed eros. Sed at neque elementum, viverra nunc quis, rutrum massa. Vivamus dictum sollicitudin laoreet. Sed ornare mollis nisl, sit amet porttitor est eleifend eu. Vestibulum pellentesque, dui vel semper eleifend, magna ligula ultrices nunc, in dignissim metus libero ut libero. Vivamus fermentum, ipsum ut tincidunt tristique, ligula sapien efficitur tellus, vitae euismod tellus risus in eros. Curabitur eget purus tincidunt dolor malesuada sagittis. Proin massa urna, pretium et bibendum a, pulvinar sit amet sapien. Nunc fringilla vitae enim non tempus.",
+      abstract : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n   Maecenas nec neque tempor velit sollicitudin dignissim. Morbi sem velit, finibus et nulla id, \n \ntempor hendrerit enim. Maecenas sit amet dolor tincidunt, aliquet orci et, bibendum mi. In placerat suscipit risus a dictum. Cras ut tortor mauris. Proin arcu odio, ornare sit amet mattis faucibus, laoreet sed eros. Sed at neque elementum, viverra nunc quis, rutrum massa. Vivamus dictum sollicitudin laoreet. Sed ornare mollis nisl, sit amet porttitor est eleifend eu. Vestibulum pellentesque, dui vel semper eleifend, magna ligula ultrices nunc, in dignissim metus libero ut libero. Vivamus fermentum, ipsum ut tincidunt tristique, ligula sapien efficitur tellus, vitae euismod tellus risus in eros. Curabitur eget purus tincidunt dolor malesuada sagittis. Proin massa urna, pretium et bibendum a, pulvinar sit amet sapien. Nunc fringilla vitae enim non tempus.",
       kind     : "Talk"
     },
     {
@@ -32,7 +32,7 @@ var program = {
         speakers  : [{
                 name          : "Francesco Zoppichino",
                 organisation  : "USI",
-                bio           : "In lacus est, lobortis nec suscipit a, imperdiet nec libero. Nulla a nibh tincidunt, placerat justo nec, maximus ante. Cras eget mi augue. Vestibulum blandit interdum velit, vitae egestas nisl euismod quis. In vitae velit sit amet odio suscipit finibus sit amet quis urna. Aenean feugiat erat quis varius cursus",
+                bio           : "In lacus est, \n lobortis nec suscipit a, imperdiet nec libero. Nulla a nibh tincidunt, placerat justo nec, maximus ante. Cras eget mi augue. Vestibulum blandit interdum velit, vitae egestas nisl euismod quis. In vitae velit sit amet odio suscipit finibus sit amet quis urna. Aenean feugiat erat quis varius cursus",
                 website       : "www.fz.ch",
                 twitter       : "@fz",
                 picture       : "http://abs.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
@@ -46,13 +46,13 @@ var program = {
       date     : new Date('Wed Nov 25 2015 08:50:00 GMT+0100 (CET)'),
       endDate  : new Date('Wed Nov 25 2015 09:00:00 GMT+0100 (CET)'),
       place    : "USI auditorium",
-      abstract : "Curabitur eu odio nec massa elementum ultrices egestas a sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec et ipsum ut nisl efficitur congue. Praesent elementum, ligula quis euismod maximus, tellus ipsum dignissim lacus, at tincidunt erat diam quis tortor.",
+      abstract : "Curabitur eu odio nec massa elementum ultrices egestas a sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec et ipsum ut nisl efficitur congue.\n Praesent elementum, ligula quis euismod maximus, tellus ipsum dignissim lacus, at tincidunt erat diam quis tortor.",
       kind     : "Talk",
       pdf      : ["http://ch.mathworks.com/moler/random.pdf", "http://ch.mathworks.com/moler/random.pdfjaflk"],
         speakers  :[{
                 name        : "Paolo Aurecchia",
                 organisation  : "USI",
-                bio           : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec neque tempor velit sollicitudin dignissim. Morbi sem velit, finibus et nulla id, tempor hendrerit enim. Maecenas sit amet dolor tincidunt, aliquet orci et, bibendum mi. In placerat suscipit risus a dictum. Cras ut tortor mauris. Proin arcu odio, ornare sit amet mattis faucibus, laoreet sed eros. Sed at neque elementum, viverra nunc quis, rutrum massa. Vivamus dictum sollicitudin laoreet. Sed ornare mollis nisl, sit amet porttitor est eleifend eu. Vestibulum pellentesque, dui vel semper eleifend, magna ligula ultrices nunc, in dignissim metus libero ut libero. Vivamus fermentum, ipsum ut tincidunt tristique, ligula sapien efficitur tellus, vitae euismod tellus risus in eros. Curabitur eget purus tincidunt dolor malesuada sagittis. Proin massa urna, pretium et bibendum a, pulvinar sit amet sapien. Nunc fringilla vitae enim non tempus.",
+                bio           : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n Maecenas nec neque tempor velit sollicitudin dignissim. Morbi sem velit, finibus et nulla id, tempor hendrerit enim.\n Maecenas sit amet dolor tincidunt, aliquet orci et, bibendum mi. In placerat suscipit risus a dictum. Cras ut tortor mauris. Proin arcu odio, ornare sit amet mattis faucibus, laoreet sed eros. Sed at neque elementum, viverra nunc quis, rutrum massa. Vivamus dictum sollicitudin laoreet. Sed ornare mollis nisl, sit amet porttitor est eleifend eu. Vestibulum pellentesque, dui vel semper eleifend, magna ligula ultrices nunc, in dignissim metus libero ut libero. Vivamus fermentum, ipsum ut tincidunt tristique, ligula sapien efficitur tellus, vitae euismod tellus risus in eros. Curabitur eget purus tincidunt dolor malesuada sagittis. Proin massa urna, pretium et bibendum a, pulvinar sit amet sapien. Nunc fringilla vitae enim non tempus.",
                 website       : "www.paoloau.ch",
                 twitter       : "@paoloau",
                 picture       : "http://abs.twimg.com/sticky/default_profile_images/default_profile_2_normal.png",
@@ -77,7 +77,7 @@ var program = {
               ref : "Luigi Frunzio"
           }
       ],
-      abstract : "Fusce sollicitudin, eros eget porta consequat, enim ipsum pretium ligula, non iaculis ipsum massa in velit. Proin nulla enim, euismod elementum sem id, congue sagittis purus. Praesent sem augue, fringilla eu sodales quis, dapibus vitae nisl.",
+      abstract : "Fusce sollicitudin, eros eget porta consequat, enim ipsum pretium ligula, non iaculis ipsum massa in velit. \n\nProin nulla enim, euismod elementum sem id, congue sagittis purus. Praesent sem augue, fringilla eu sodales quis, dapibus vitae nisl.",
       kind     : "Talk",
       pdf      : ["http://www.cypherpunks.to/~peter/06_random.pdf"]
     }
@@ -107,7 +107,7 @@ var program = {
         speakers  : [{
                 name          : "Davide Cammarata",
                 organisation  : "USI",
-                bio           : "Fusce ultricies massa id elit scelerisque rhoncus. Vivamus mollis laoreet nunc ut volutpat. Duis vestibulum eros velit, id scelerisque tortor porta ac. Vivamus sit amet elementum enim. Etiam sed lectus pulvinar, placerat massa at, vehicula tortor. Pellentesque faucibus felis velit, ac vehicula est posuere a. Phasellus a neque at elit condimentum condimentum volutpat eget velit. Etiam commodo nulla lorem, ac varius lacus aliquam ut. Nunc sed massa nec erat pulvinar aliquam. Vivamus ornare dui at pulvinar pellentesque. Aliquam a viverra nulla.",
+                bio           : "Fusce ultricies massa id elit scelerisque rhoncus. Vivamus mollis laoreet nunc ut volutpat. Duis vestibulum eros velit, id scelerisque tortor porta ac.\n Vivamus sit amet elementum enim. Etiam sed lectus pulvinar, placerat massa at, vehicula tortor. Pellentesque faucibus felis velit, ac vehicula est posuere a. Phasellus a neque at elit condimentum condimentum volutpat eget velit. Etiam commodo nulla lorem, ac varius lacus aliquam ut. Nunc sed massa nec erat pulvinar aliquam.\n Vivamus ornare dui at pulvinar pellentesque. Aliquam a viverra nulla.",
                 website       : "www.cammarata.ch",
                 twitter       : "@dcamma",
                 picture       : "http://abs.twimg.com/sticky/default_profile_images/default_profile_1_normal.png2",
@@ -125,7 +125,7 @@ var program = {
                 ref: "Davide Cammarata"},
                 {ref: "Marco Calzana"}
       ],
-      abstract : "Fusce ultricies massa id elit scelerisque rhoncus. Vivamus mollis laoreet nunc ut volutpat. Duis vestibulum eros velit, id scelerisque tortor porta ac. Vivamus sit amet elementum enim. Etiam sed lectus pulvinar, placerat massa at, vehicula tortor. Pellentesque faucibus felis velit, ac vehicula est posuere a. Phasellus a neque at elit condimentum condimentum volutpat eget velit. Etiam commodo nulla lorem, ac varius lacus aliquam ut. Nunc sed massa nec erat pulvinar aliquam. Vivamus ornare dui at pulvinar pellentesque. Aliquam a viverra nulla.",
+      abstract : "Fusce ultricies massa id elit scelerisque rhoncus. Vivamus mollis laoreet nunc ut volutpat. Duis vestibulum eros velit, id scelerisque tortor porta ac. Vivamus sit amet elementum enim. Etiam sed lectus pulvinar, placerat massa at, vehicula tortor. Pellentesque faucibus felis velit, ac vehicula est posuere a. Phasellus a neque at elit condimentum condimentum volutpat eget velit. Etiam commodo nulla lorem, ac varius lacus aliquam ut. Nunc sed massa nec erat pulvinar aliquam.\n Vivamus ornare dui at pulvinar pellentesque.\n Aliquam a viverra nulla.",
       kind     : "Talk"
     }
   ]
