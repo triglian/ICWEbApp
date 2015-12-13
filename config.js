b7044bfefb799799d98411c3cab33ae3bcf58579
Federico Pfahler <pfahler.federico@gmail.com>
2015-12-13 14:55:10 +0100

    Now form is hidden after commit, still needed to reload page to make it disappear

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ module.exports = {
     "websiteTitle": "ICWE2016",
     "email": "cesare.pautasso@usi.ch",
     "description": "The 16th International Conference on Web Engineering (ICWE2016) will be held from Monday, June 6th until Thursday, June 9th 2016 at the USI Faculty of Informatics, Lugano, Switzerland.\n ICWE aims to bring together researchers and practitioners from industry and academia to tackle emerging and interdisciplinary challenges in the engineering of Web applications using next-generation technologies methods and tools which are becoming every day more critical for our society, education, media and culture.",
-    "location": "[46.010552, 8.958109]",
+    "location": "[46.010782, 8.958109]",
     "twitterMainID": [507663518],
     "twitterMain": ["@GiggiuxF"],
     "twitterFeeds": ["#ICWE", "#ICWE2016", "@pautasso", "@USInews"]
